96bb77245fbe09be2517c53da4a88fc2fff57666
Ryan Guo <gryan@outlook.com.au>
2025-05-04 18:22:20 +1000

    Routing

---
+++ app/routes/admin/admin-layout.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Outlet} from "react-router";
+
+const AdminLayout = () => {
+    return (
+        <div className="admin-layout">AdminLayout
+        <aside className="children">
+            <Outlet />
+        </aside>
+        </div>
+    )
+}
+export default AdminLayout
