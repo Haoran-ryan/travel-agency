1cec6aec5b005ed70599be486a70b54266297193
Ryan Guo <gryan@outlook.com.au>
2025-05-11 14:49:43 +1000

    dashboard admin ui done

--- app/routes/admin/dashboard.tsx
+++ app/routes/admin/dashboard.tsx
@@ -1,21 +1,22 @@
 import React from 'react'
 import {Header, StatsCard, TripCard} from "../../../components";
-import { dashboardStats, allTrips, users } from '../../constants';
-const user = { name: 'Ryan Guo'}
-const { totalUsers, usersJoined, totalTrips, tripsCreated, userRole } = dashboardStats
+import {dashboardStats, allTrips, users} from '../../constants';
+
+const user = {name: 'Ryan Guo'}
+const {totalUsers, usersJoined, totalTrips, tripsCreated, userRole} = dashboardStats
 
 const Dashboard = () => {
 
     return (
         <main className='dashboard wrapper'>
             <Header
-                title={`Welcome ${user?.name?? 'Guest'}`}
+                title={`Welcome ${user?.name ?? 'Guest'}`}
                 description='Track activity, trends and popular destiantions in real time'
             />
             <section className='flex flex-col gap-6'>
                 <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
                     <StatsCard
-                        headerTitle = 'Total Trips'
+                        headerTitle='Total Trips'
                         total={totalTrips}
                         currentMonthCount={tripsCreated.currentMonthCount}
                         lastMonthCount={tripsCreated.lastMonthCount}
@@ -40,13 +41,13 @@ const Dashboard = () => {
             <section className='container'>
                 <h1 className='text-xl font-semibold text-dark-100'>Created Trips</h1>
                 <div className='trip-grid'>
-                    {allTrips.slice(0, 4).map(({id, name, imageUrls, itinerary, tags, estimatedPrice} ) => (
+                    {allTrips.slice(0, 4).map(({id, name, imageUrls, itinerary, tags, estimatedPrice}) => (
                         <TripCard
                             key={id}
                             id={id.toString()}
                             name={name}
-                            imageUrls={imageUrls[0]}
-                            location={itinerary?.[0]?.location??''}
+                            imageUrl={imageUrls[0]}
+                            location={itinerary?.[0]?.location ?? ''}
                             tags={tags}
                             price={estimatedPrice}
                         />
